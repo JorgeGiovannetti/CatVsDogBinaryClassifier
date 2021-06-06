87a982ed7b1618ac985f36866fda1ebd6868b973
JorgeGiovannetti <giovannetti2000@gmail.com>
2021-06-06 15:34:19 -0500

    Fixed bug by casting y_output to uint16

--- load_dataset.py
+++ load_dataset.py
@@ -5,12 +5,13 @@ import matplotlib.pyplot as plt
 from tqdm import tqdm
 
 
-def load_data(dataset):
+def load_data(dataset, img_size = 100):
     """
     Loads images from dataset and returns it in the desired format as numpy arrays
 
     Arguments:
-    dataset -- dataset folder (dev_set, test_set, or training_set)
+    dataset -- dataset folder ('dev_set', 'test_set', or 'training_set')
+    img_size (default: 100) -- size to resize images
 
     Returns:
     x_output -- numpy array of features (num_samples, img_size, img_size, 3)
@@ -21,21 +22,20 @@ def load_data(dataset):
     np.random.seed(1)
 
     dirname = os.path.dirname(os.path.realpath('__file__'))
-    img_size = 100
+    
     categories = ['cat', 'dog']
 
     dataset_array = []
 
-    for category in categories:
+    for class_num in range(len(categories)):
+        category = categories[class_num]
         path = os.path.join(dirname, 'dataset', dataset, category)
-        class_num = categories.index(category)
 
         for img in tqdm(os.listdir(path)):
             try:
                 # Read image from file into array
                 img_array = cv2.imread(
                     os.path.join(path, img), cv2.IMREAD_COLOR)
-
                 # Resize image to (IMG_SIZE, IMG_SIZE)
                 img_resized = cv2.resize(img_array, (img_size, img_size))
                 dataset_array.append([img_resized, class_num])
@@ -53,19 +53,6 @@ def load_data(dataset):
 
     # Reshape arrays
     x_output = np.stack(x_output)
-    y_output = y_output.reshape((1, y_output.shape[0]))
+    y_output = y_output.reshape((1, y_output.shape[0])).astype(np.uint16)
 
     return x_output, y_output, categories
-
-
-# Load Data Demo
-dev_x_orig, dev_y, classes = load_data('dev_set')
-
-# Explore your dataset 
-m_train = dev_x_orig.shape[0]
-num_px = dev_x_orig.shape[1]
-
-print ("Number of dev examples: " + str(m_train))
-print ("Each image is of size: (" + str(num_px) + ", " + str(num_px) + ", 3)")
-print ("dev_x_orig shape: " + str(dev_x_orig.shape))
-print ("dev_y shape: " + str(dev_y.shape))
